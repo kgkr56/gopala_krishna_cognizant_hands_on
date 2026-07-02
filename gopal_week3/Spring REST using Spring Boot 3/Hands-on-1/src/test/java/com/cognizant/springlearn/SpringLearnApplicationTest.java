7b369b713a153d8048b3ad3314fa5e68772bd7cd
gopal56 <kgkreddy56@gmail.com>
2026-07-02 18:44:22 +0530

    Add Spring REST Hands-on-1 and Hands-on-4 (Week 3)

---
+++ gopal_week3/Spring REST using Spring Boot 3/Hands-on-1/src/test/java/com/cognizant/springlearn/SpringLearnApplicationTest.java
@@ -0,0 +1,13 @@
+package com.cognizant.springlearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLearnApplicationTest {
+
+    @Test
+    void contextLoads() {
+        System.out.println("Spring context loaded successfully!");
+    }
+}
