6269c1508413a78c7fed1be93ce1320c05bd727b
gopal56 <kgkreddy56@gmail.com>
2026-07-06 17:08:31 +0530

    feat: add Country Web Service REST API returning India details

---
+++ gopal_week3/Spring REST using Spring Boot 3/Country Web Service/src/main/java/com/cognizant/springlearn/controller/CountryController.java
@@ -0,0 +1,18 @@
+package com.cognizant.springlearn.controller;
+
+import com.cognizant.springlearn.Country;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CountryController {
+
+    @GetMapping("/country")
+    public Country getCountryIndia() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("country.xml");
+        Country country = (Country) context.getBean("india");
+        return country;
+    }
+}
